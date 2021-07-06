5ff772241af1fe40297baa7b73256a7cbc0a6975
zeus <rootapostolos@swarmlab.io>
2021-07-06 17:09:33 +0300

    docker tools

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+docker stop swarmlab-antora
+docker container rm swarmlab-antora
 docker pull hub.swarmlab.io:5480/antora 
 docker run -ti --name swarmlab-antora -v $PWD/docs:/antora hub.swarmlab.io:5480/antora  /bin/sh -c "build.sh"
