c77092fb432239ff9e7ed3c57e8a4b4f243bb543
zeus <rootapostolos@swarmlab.io>
2021-07-06 18:44:23 +0300

    add docker

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker stop swarmlab-antora
 docker container rm swarmlab-antora
 docker pull hub.swarmlab.io:5480/antora 
 #docker run -ti --name swarmlab-antora -v $PWD/docs:/antora hub.swarmlab.io:5480/antora  /bin/sh -c "build.sh"
-docker run -ti --name swarmlab-antora -v $PWD/docs:/antora hub.swarmlab.io:5480/antora  /bin/sh -c "export NODE_PATH=/usr/lib/node_modules:$NODE_PATH exec sh -c 'build.sh'"
+docker run -ti --name swarmlab-antora -v $PWD/docs:/antora hub.swarmlab.io:5480/antora  /bin/sh -c "NODE_PATH=/usr/lib/node_modules:\$NODE_PATH exec sh -c 'build.sh'"
