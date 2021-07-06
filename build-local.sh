0908cf3f897f872ef013297a65ff034c00c35ab5
zeus <rootapostolos@swarmlab.io>
2021-07-06 18:01:03 +0300

    docker tools

--- build-local.sh
+++ build-local.sh
@@ -1,4 +1,5 @@
 docker stop swarmlab-antora
 docker container rm swarmlab-antora
 docker pull hub.swarmlab.io:5480/antora
-docker run -ti --name swarmlab-antora  -v $PWD/docs:/antora -v $PWD:/doc-repo hub.swarmlab.io:5480/antora  /bin/sh -c "build-local.sh"
+#docker run -ti --name swarmlab-antora  -v $PWD/docs:/antora -v $PWD:/doc-repo hub.swarmlab.io:5480/antora  /bin/sh -c "build-local.sh"
+docker run -ti --name swarmlab-antora  -v $PWD/docs:/antora -v $PWD:/doc-repo hub.swarmlab.io:5480/antora  /bin/sh -c "export NODE_PATH=/usr/lib/node_modules:$NODE_PATH exec sh -c 'build.sh'"
