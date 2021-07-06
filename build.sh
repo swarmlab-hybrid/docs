96b8323eaa6df4ad4111dd6378cd00d04abed0ba
zeus <rootapostolos@swarmlab.io>
2021-07-07 00:43:05 +0300

    ui

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ docker stop swarmlab-antora
 docker container rm swarmlab-antora
 docker pull hub.swarmlab.io:5480/antora 
 #docker run -ti --name swarmlab-antora -v $PWD/docs:/antora hub.swarmlab.io:5480/antora  /bin/sh -c "build.sh"
-docker run -ti --name swarmlab-antora -v $PWD/docs:/antora hub.swarmlab.io:5480/antora  /bin/sh -c "NODE_PATH=/usr/local/lib/node_modules:\$NODE_PATH exec sh -c 'build.sh'"
+docker run -ti --name swarmlab-antora -v $PWD/docs:/antora hub.swarmlab.io:5480/antora  /bin/sh -c "DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr DOCSEARCH_INDEX_VERSION=latest NODE_PATH=/usr/local/lib/node_modules:\$NODE_PATH exec sh -c 'build.sh'"
